00f8d88ae3464cb9e34626029b313ce3b31046df
nugliar <hashan.hall@gmail.com>
2021-10-12 16:39:34 -0500

    add translator logic

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,84 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+  invertDictionary(dictionaries) {
+    if (dictionaries == null) {
+      return null;
+    }
+    if (Array.isArray(dictionaries)) {
+      let invertedDictionaries = [];
+      for (const dictionary of dictionaries) {
+        invertedDictionaries.push(invertDictionary(dictionary))
+      }
+      return invertedDictionaries;
+    }
+    if (typeof dictionaries == 'object') {
+      const dictionary = dictionaries;
+      const invertedDictionary = dictionary.reduce((object, key) => {
+        object[dictionary[key]] = key;
+        return object;
+      }, {});
+      return invertDictionary;
+    }
+    return {};
+  }
+
+  translate(wordString, locale) {
+
+    const translateWords = (words, dictionaries) => {
+      let wordsArr = words.slice();
+
+      for (let numWords = wordsArr.length; numWords > 0; numWords--) {
+        for (let start = 0;;) {
+          for (const dictionary of dictionaries) {
+
+            const phrase = wordsArr.slice(start, numWords).join('');
+
+            if (dictionary[phrase]) {
+              wordsArr = wordsArr
+                .slice(0, start)
+                .concat(dictionary[phrase].split(''))
+                .concat(wordsArr.slice(start + numWords))
+              start += numWords;
+            } else {
+              start++;
+            }
+
+            if (start <= wordsArr.length - numWords) {
+              break;
+            }
+          }
+        }
+      }
+      
+      return wordsArr;
+    }
+
+    let dictionaries = null;
+
+    if (locale == 'American to British') {
+      dictionaries = [
+        americanToBritishSpelling,
+        americanToBritishTitles,
+        americanOnly
+      ]
+    } else if (locale == 'British to American') {
+      dictionaries = [
+        this.invertDictionary(americanToBritishSpelling),
+        this.invertDictionary(americanToBritishTitles),
+        britishOnly
+      ]
+    }
+
+    if (!dictionaries) {
+      throw new Error('Invalid locale');
+    }
+
+    const words = wordString.split('');
+    const translatedWords = this.translateWords(words, dictionaries);
+
+    return translatedWords.join('');
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,20 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const locale = req.body.locale;
+      const text = req.body.text;
+
+      if (!locale || !text) {
+        return res.json({ error: 'fuck you' });
+      }
+
+      const translation = translator.translate(text, locale);
+
+      res.json({ translation: translation });
     });
 };
